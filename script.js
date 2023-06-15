0ed13ec9a20a934bcbc6da2a1b226979d46eeb53
TOUFIK SK <toufiksk.ab3@gmail.com>
2023-06-15 16:55:37 +0530

    Design fix , fix page Responsive
    Design fix , fix page Responsive

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var popupContainer = document.querySelector(".popup-container");
+
+  // Add 'active' class to the popup container to show the popup
+  function showPopup() {
+    popupContainer.classList.add("active");
+  }
+
+  // Remove 'active' class to hide the popup
+  function hidePopup() {
+    popupContainer.classList.remove("active");
+  }
+
+  // Show the popup when the page loads
+  showPopup();
+
+  // Close the popup when clicking outside the image
+  popupContainer.addEventListener("click", hidePopup);
+  // Prevent the click event on the image from closing the popup
+  popupContainer.querySelector("img").addEventListener("click", function(event) {
+    event.stopPropagation();
+  });
+});
