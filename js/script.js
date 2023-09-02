5e48c10c1355c22567b9ee356a886a32ff52be84
TOUFIK SK <toufiksk.ab3@gmail.com>
2023-09-03 01:04:25 +0530

    Add to cart button added
    & Show alert and added item to cart

--- js/script.js
+++ js/script.js
@@ -9,35 +9,42 @@ btnNavEl.addEventListener("click", function () {
 
 
 
-// Sticky navigation
-const sectionHeroEl = document.querySelector(".section-hero");
+// // Sticky navigation
+// const sectionHeroEl = document.querySelector(".section-hero");
 
-const obs = new IntersectionObserver(
-  function (entries) {
-    const ent = entries[0];
+// const obs = new IntersectionObserver(
+//   function (entries) {
+//     const ent = entries[0];
 
-    if (ent.isIntersecting === false) {
-      document.body.classList.add("sticky");
-    }
+//     if (ent.isIntersecting === false) {
+//       document.body.classList.add("sticky");
+//     }
 
-    if (ent.isIntersecting === true) {
-      document.body.classList.remove("sticky");
-    }
-  },
-  {
-    // In the viewport
-    root: null,
-    threshold: 0,
-    rootMargin: "-80px",
+//     if (ent.isIntersecting === true) {
+//       document.body.classList.remove("sticky");
+//     }
+//   },
+//   {
+//     // In the viewport
+//     root: null,
+//     threshold: 0,
+//     rootMargin: "-120px",
+//   }
+// );
+// obs.observe(sectionHeroEl);
+
+
+
+
+
+  const toastTrigger = document.getElementById('liveToastBtn')
+  const toastLiveExample = document.getElementById('liveToast')
+  
+  if (toastTrigger) {
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+    toastTrigger.addEventListener('click', () => {
+      toastBootstrap.show()
+    })
   }
-);
-obs.observe(sectionHeroEl);
 
-
-
-// add to cart
-
-let addCart=(()=>{
-  console.log("added to cart")
-})
 
