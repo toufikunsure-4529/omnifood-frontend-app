6ea66e61c90a02ae27aadf05903ad7c2186ac8c7
TOUFIK SK <toufiksk.ab3@gmail.com>
2023-10-21 02:19:36 +0530

    REMOVED ORDER WISE PRODUCT VIEW EDOMINER TIME ADDED

--- js/script.js
+++ js/script.js
@@ -2,20 +2,19 @@
 // Make mobile navigation work
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
-
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
 
 
+
+
 // Sticky navigation
-
 const sectionHeroEl = document.querySelector(".section-hero");
-
 const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
-    console.log(ent);
+    // console.log(ent);
 
     if (ent.isIntersecting === false) {
       document.body.classList.add("sticky");
@@ -35,33 +34,10 @@ const obs = new IntersectionObserver(
 obs.observe(sectionHeroEl);
 
 
-// add to cart live toast bootstarp
 
-  const toastTrigger = document.getElementById('liveToastBtn')
-  const toastLiveExample = document.getElementById('liveToast')
-  
-  if (toastTrigger) {
-    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
-    toastTrigger.addEventListener('click', () => {
-      toastBootstrap.show()
-    })
-  }
-
-
-  // login button click 
-
-  let loginBtn=document.getElementById("loginBtn")
-  let showMsg=document.getElementById("showMsg")
-  loginBtn.addEventListener("click",function(){
-    showMsg.innerHTML="Login features are coming soon. Sorry for the inconvenience..."
-  })
-
-
+// CTA FORM PHONE NUMBER validatePhoneNumber
   function validatePhoneNumber(input) {
-    // only added numaric non numeric removed characters from the input
     const numericInput = input.value.replace(/[^0-9]/g, '');
-
-    // Limit the input to a maximum of 10 digits
     const maxLength = 10;
     if (numericInput.length > maxLength) {
         input.value = numericInput.slice(0, maxLength);
@@ -71,7 +47,4 @@ obs.observe(sectionHeroEl);
     }
 }
 
-let browseProd=document.getElementById("browseProd")
-browseProd.onclick = function () {
-  location.href = "https://getbootstrap.com/docs/5.3/components/buttons/#base-class";
-};
+
